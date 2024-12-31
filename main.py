9e394efeb496246b7512fb445d028aa2af7745a6
root <root@laptop.>
2024-12-31 15:36:24 -0600

    B: created main function to open/read file. Create count_words function to count words

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("hello world")
+def main():
+    with open("books/frankenstein.txt") as f:
+        file_contents = f.read()
+        
+def count_words():
+    count = 0
+    with open("books/frankenstein.txt") as f:
+        words_arr = f.read().split()
+    for word in words_arr:
+        count += 1
+    print(count)
+
+    
+count_words()
+
+main()
