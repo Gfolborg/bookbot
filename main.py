326154844bc409164c2d06a97bae0d128d37a85e
root <root@laptop.>
2025-01-03 06:56:10 -0600

    finished up main function. project complete

--- main.py
+++ main.py
@@ -1,28 +1,40 @@
-def open_book():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-        return file_contents.lower()
+def book_file():
+    try:
+        with open("books/frankenstein.txt") as f:
+            file_contents = f.read()
+            return file_contents.lower()
+    except FileNotFoundError:
+        print("File did not open. Check file")
+    
 
-def count_words(book): #boots, quick question in a question. Should I replace the parameter with book_content?
-    return len(book.split())
+def word_count(text):
+    words = book_file()
+    return len(words.split())
 
-
-def count_characters(words):
+def letter_count(text):
     char_count = {}
-    for char in words:
-        if char not in char_count:
-            char_count[char] = 1
-        else:
-            char_count[char] += 1
+    for char in text:            
+        if char.isalpha():      
+            char = char.lower()   
+            if char not in char_count:
+                char_count[char] = 1
+            else:
+                char_count[char] += 1
     return char_count
+    
 
+    
+    
 def main():
-    inside_book = open_book()
-    number_of_words = count_words(inside_book)
-    character_count = count_characters(inside_book)
+    book = book_file()
+    number_of_words = word_count(book)
+    number_of_letters = letter_count(book)
+
     print("--- Begin report of books/frankenstein.txt ---")
-    print(f"{number_of_words} words found in the document")
-    print("\nCharacter counts: ")
-    print(character_count)
-if __name__ == "__main__":
-    main() 
+    print(f"{number_of_words} words found in the document/n")
+
+    for char in number_of_letters:
+        print(f"The '{char}' character was found {number_of_letters[char]} times")
+
+
+(main())
