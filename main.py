ef6d2f71bb66921d9fc54b7a6beb63f5eeda5137
root <root@laptop.>
2025-01-03 17:15:01 -0600

    added sort function

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def word_count(text):
 
 def letter_count(text):
     char_count = {}
-    for char in text:            
+    for char in text:                        
         if char.isalpha():      
             char = char.lower()   
             if char not in char_count:
@@ -21,20 +21,26 @@ def letter_count(text):
             else:
                 char_count[char] += 1
     return char_count
-    
 
-    
+def sort(s):
+    return s["count"]
+
+def sorted_list(text):
+    list_items = []
+    for char, count in text.items():
+        list_items.append({"char":char, "count":count})
+    list_items.sort(reverse=True, key=sort)
+    return list_items
     
 def main():
     book = book_file()
     number_of_words = word_count(book)
     number_of_letters = letter_count(book)
+    sorted_char_list = sorted_list(number_of_letters)
 
     print("--- Begin report of books/frankenstein.txt ---")
-    print(f"{number_of_words} words found in the document/n")
+    print(f"{number_of_words} words found in the document\n")
 
-    for char in number_of_letters:
-        print(f"The '{char}' character was found {number_of_letters[char]} times")
-
-
-(main())
+    for letter in sorted_char_list:
+        print(f"The '{letter["char"]}' character was found {letter["count"]}")  
+main()
